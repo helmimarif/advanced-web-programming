9906910508657c2f3fea0fc27e813809d9774dd2
Ayse <106028948+helmimarif@users.noreply.github.com>
2022-05-23 12:27:59 +0700

    PHP

---
+++ Praktikum 1/websitephp/contact.php
@@ -0,0 +1,12 @@
+<div class="page">
+    <h2>Hubungi Kami</h2>
+    <p>Telp: 0274-2885822
+        <br>WA: 0895395186038
+        <br>Senin - Minggu
+    24 Jam Non Stop</p>
+ 
+    <p>Jl. Palagan Tentara Pelajar
+        No 81 Jongkang, Sariharjo, Ngaglik, Sleman
+        Daerah Istimewa Yogyakarta
+    55581</p>
+</div>
